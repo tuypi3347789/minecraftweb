60ed4d0edc339b8d805b451b1bdb51725b21b337
tuypi3347789 <tuypi3347789@gmail.com>
2020-09-03 03:03:59 +0800

    申請帳號判斷

--- src/views/Home/Page/loginPage/index.vue
+++ src/views/Home/Page/loginPage/index.vue
@@ -14,7 +14,7 @@
                 </router-link>
               </div>
               <div class="col-5">
-                <button @click="view = 'register'" class="btn w-50 h-100 mr-2 pull-right border border-dark">
+                <button @click="view = 'register'" class="btn w-50 h-100 mr-4 pull-right border border-dark">
                   <span>註冊</span>
                 </button>
               </div>
@@ -59,7 +59,7 @@
                 </router-link>
               </div>
               <div class="col-5">
-                <button @click="view = 'login'" class="btn w-50 h-100 mr-2 pull-right border border-dark">
+                <button @click="view = 'login'" class="btn w-50 h-100 mr-4 pull-right border border-dark">
                   <span>登入</span>
                 </button>
               </div>
@@ -70,27 +70,38 @@
               </div>
             </div>
             <div class="row justify-content-center" style="line-height: 5vmin">
+              <label class="col-2 text-right">名字:</label>
+              <div class="col-4">
+                <input type="text" placeholder="請跟遊戲名字一樣" class="form-control h-100" v-model="group.playerName" />
+              </div>
+            </div>
+            <div class="row justify-content-center" style="line-height: 5vmin">
               <label class="col-2 text-right">帳號:</label>
               <div class="col-4">
-                <input type="text" class="form-control h-100" />
+                <input type="text" placeholder="最少需6個英數" @change="length(group.playerAccount)"
+                  class="form-control h-100" v-model="group.playerAccount" />
               </div>
             </div>
             <div class="row justify-content-center" style="line-height: 5vmin">
               <label class="col-2 text-right">密碼:</label>
               <div class="col-4">
-                <input type="text" class="form-control h-100" />
+                <input type="password" placeholder="最少需6個英數" @change="length(group.playerPassword)"
+                  class="form-control h-100" v-model="group.playerPassword" />
               </div>
             </div>
             <div class="row justify-content-center" style="line-height: 5vmin">
               <label class="col-2 text-right">密碼確認:</label>
               <div class="col-4">
-                <input type="text" class="form-control h-100" />
+                <input type="password" placeholder="請跟上方一樣" @change="length(password)"
+                  class="form-control h-100" v-model="password"
+                  :class="[ group.playerPassword !== password ? 'border-bottom border-danger' : '']" />
               </div>
             </div>
             <div class="row justify-content-center" style="line-height: 5vmin">
               <label class="col-2 text-right">電子郵件:</label>
               <div class="col-4">
-                <input type="text" class="form-control h-100" />
+                <input type="email" placeholder="忘記密碼可使用" class="form-control h-100" v-model="group.playerEmail"
+                  @change="emaillChange(group.playerEmail)" />
               </div>
             </div>
             <div class="row justify-content-center" style="line-height: 5vmin">
@@ -123,16 +134,20 @@ export default {
     return {
       height: '',
       view: 'login',
-      array: [
-        {
-          Id: 1,
-          playerName: 'test123',
-          playerEmail: 'test@gmail.com',
-          playerAccount: 'root',
-          playerPassword: 'abc123',
-        },
-      ],
+      group: {
+        playerAccount: '',
+        playerPassword: '',
+        playerEmail: '',
+        playerName: '',
+      },
+      password: '',
       value: '',
+      test: [
+        {
+          uuid: '1-2-3-4',
+          name: 'test',
+        }
+      ]
     };
   },
   methods: {
@@ -140,18 +155,53 @@ export default {
       console.log(window.innerHeight);
       this.height = window.innerHeight ;
     },
+    length(value) {
+      const arr = value.split('');
+      if (arr.length < 6) {
+        alert('請檢查是否超過6個');
+        return false;
+      }
+      return true;
+    },
     register() {
-      axios({
-        method: 'post',
-        baseURL: 'https://localhost:5001',
-        url: '/api/Player/register',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        data: this.array[0],
-      })
-        .then((result) => { console.log(result.data) })
-        .catch((err) => { console.error(err) })
+      console.log(this.group);
+      if (this.group.playerAccount !== '' && this.group.playerPassword !== ''
+        && this.group.playerName !== '' && this.group.playerEmail !== '') {
+          if (this.length(this.group.playerAccount) && this.length(this.group.playerPassword)) {
+            axios({
+              method: 'post',
+              baseURL: 'https://localhost:5001',
+              url: '/api/Player/register',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              data: this.group,
+            })
+              .then((result) => { 
+                console.log(result);
+                if (result.data === 1) {
+                  alert('申請失敗，名字已被使用過!!');
+                }
+                if (result.data === 2) {
+                  alert('申請失敗，電子郵件已申請過!!');
+                }
+                if (result.data === 3) {
+                  alert('申請成功');
+                }
+              })
+              .catch((err) => { console.error(err) })
+          }
+        } else {
+          alert('欄位不得為空');
+        }
+    },
+    emaillChange(value) {
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      
+      if (!re.test(value)) {
+        this.group.playerEmail = '';
+        alert('電子郵件格式不正確');
+      }
     },
     goHome() {
       this.$router.go("Home");
@@ -159,14 +209,15 @@ export default {
   },
   mounted() {
     this.getHeight();
+    console.log(JSON.stringify(this.test));
     
   },
 };
 </script>
 <style>
 .loginPage .loginDiv {
-  padding-top: 5vmin;
-  padding-bottom: 5vmin;
+  padding-top: 4vmin;
+  padding-bottom: 4vmin;
   box-shadow: 1vmin 1vmin 1vmin #333;
   z-index: 9999;
   width: 100%;
@@ -178,7 +229,7 @@ export default {
 .loginDiv input {
   border:0;
   border-bottom: .2vmin solid #777;
-  font-size: 3vmin;
+  font-size: 2vmin;
   border-radius: 0;
 }
 .loginPage {
